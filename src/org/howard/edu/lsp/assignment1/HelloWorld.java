dc2c97629b0d8deb7187a61820361874e84e784a
Maize Booker <mbooker734@gmail.com>
2023-08-31 22:45:54 -0400

    First Commit

---
+++ src/org/howard/edu/lsp/assignment1/HelloWorld.java
@@ -0,0 +1,11 @@
+/**
+* Name: Maize Booker
+*/
+
+package org.howard.edu.lsp.assignment1;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello World!!");
+	}
+}
